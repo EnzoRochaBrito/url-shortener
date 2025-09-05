31a71de190354ae4d0cd6a76361d71c3d98eb51a
enzo <enzo1brich@gmail.com>
2025-09-05 15:29:15 -0300

    removing sqlite from DBTypes

--- src/db/db.factory.ts
+++ src/db/db.factory.ts
@@ -1,16 +1,13 @@
 import { IUrlShortenerDB } from "./db.interface";
 import { UrlPostgreDB } from "./db.postgresql";
 
-type DBTypes = 'postgre' | 'sqlite'
+type DBTypes = 'postgre'
 
 export class DBFactory {
     public static async build(db: DBTypes = 'postgre'): Promise<IUrlShortenerDB> {
         switch (db) {
             case 'postgre':
                 return await new UrlPostgreDB().init();
-                break;
-            
-            case 'sqlite':
                 break;
         }
         return new UrlPostgreDB()
